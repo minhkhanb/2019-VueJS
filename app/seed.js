cf24e06eb315174ada9f5dcc98238e01740f31c9
Kha Le <minhkhanb@gmail.com>
2019-03-11 15:27:48 +0700

    init

---
+++ app/seed.js
@@ -0,0 +1,40 @@
+const Seed = {
+  submissions: [
+    {
+      id: 0,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 11,
+      avatar: '../public/images/luffy.jpg',
+      submissionImage: '../public/images/luffy.jpg'
+    },
+    {
+      id: 1,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 15,
+      avatar: '../public/images/luffy.jpg',
+      submissionImage: '../public/images/luffy.jpg'
+    },
+    {
+      id: 2,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 16,
+      avatar: '../public/images/luffy.jpg',
+      submissionImage: '../public/images/luffy.jpg'
+    },
+    {
+      id: 3,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 12,
+      avatar: '../public/images/luffy.jpg',
+      submissionImage: '../public/images/luffy.jpg'
+    }
+  ]
+};
